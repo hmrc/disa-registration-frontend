dbd7acd6e573297422a390033e1f11972cfbb8bd
Tom Robinson <25112739+magicaljellybeans@users.noreply.github.com>
2025-09-17 09:38:14 +0100

    DFI-942 Comment out navigation test for RegissteredIsaManager page for time being

--- test/navigation/NavigatorSpec.scala
+++ test/navigation/NavigatorSpec.scala
@@ -45,10 +45,10 @@ class NavigatorSpec extends SpecBase {
           .onPageLoad()
       }
     }
-
-    "must navigate correctly from RegisteredIsaManager page" in {
-      navigator.nextPage(RegisteredIsaManagerPage, NormalMode, UserAnswers("id")) mustBe ???
-      navigator.nextPage(RegisteredIsaManagerPage, CheckMode, UserAnswers("id")) mustBe ???
-    }
+// TODO uncomment when next page available
+//    "must navigate correctly from RegisteredIsaManager page" in {
+//      navigator.nextPage(RegisteredIsaManagerPage, NormalMode, UserAnswers("id")) mustBe ???
+//      navigator.nextPage(RegisteredIsaManagerPage, CheckMode, UserAnswers("id")) mustBe ???
+//    }
   }
 }
