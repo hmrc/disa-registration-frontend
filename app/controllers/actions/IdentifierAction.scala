22d605f5c5fcd59a4d5ec1b0aefed1d0100be0fc
Tom Robinson <25112739+magicaljellybeans@users.noreply.github.com>
2025-09-24 11:37:09 +0100

    DFI-943 Ensure kickout on wrong affinity group to GG sign-in

--- app/controllers/actions/IdentifierAction.scala
+++ app/controllers/actions/IdentifierAction.scala
@@ -20,9 +20,10 @@ import com.google.inject.Inject
 import config.FrontendAppConfig
 import controllers.routes
 import models.requests.IdentifierRequest
-import play.api.mvc.Results._
-import play.api.mvc._
-import uk.gov.hmrc.auth.core._
+import play.api.mvc.Results.*
+import play.api.mvc.*
+import uk.gov.hmrc.auth.core.*
+import uk.gov.hmrc.auth.core.AffinityGroup.Organisation
 import uk.gov.hmrc.auth.core.retrieve.v2.Retrievals
 import uk.gov.hmrc.http.{HeaderCarrier, UnauthorizedException}
 import uk.gov.hmrc.play.http.HeaderCarrierConverter
@@ -45,14 +46,16 @@ class AuthenticatedIdentifierAction @Inject() (
 
     implicit val hc: HeaderCarrier = HeaderCarrierConverter.fromRequestAndSession(request, request.session)
 
-    authorised().retrieve(Retrievals.internalId) {
+    authorised(Organisation).retrieve(Retrievals.internalId) {
       _.map { internalId =>
         block(IdentifierRequest(request, internalId))
       }.getOrElse(throw new UnauthorizedException("Unable to retrieve internal Id"))
     } recover {
-      case _: NoActiveSession        =>
+      case _: UnsupportedAffinityGroup =>
         Redirect(config.loginUrl, Map("continue" -> Seq(config.loginContinueUrl)))
-      case _: AuthorisationException =>
+      case _: NoActiveSession          =>
+        Redirect(config.loginUrl, Map("continue" -> Seq(config.loginContinueUrl)))
+      case _: AuthorisationException   =>
         Redirect(routes.UnauthorisedController.onPageLoad())
     }
   }
