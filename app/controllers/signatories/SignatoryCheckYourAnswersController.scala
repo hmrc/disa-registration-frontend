c33bc2ddfe3cf5d5501290cc5ad4d2676ae9cc47
zach.hall <29452017+zachhall1234@users.noreply.github.com>
2026-04-10 10:57:01 +0100

    DFI-1741 Create 'Check added signatory' screen

---
+++ app/controllers/signatories/SignatoryCheckYourAnswersController.scala
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.signatories
+
+import controllers.actions.*
+import models.journeydata.signatories.Signatory
+import models.requests.DataRequest
+import play.api.Logging
+import play.api.i18n.{I18nSupport, Messages, MessagesApi}
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
+import viewmodels.checkAnswers.signatories.{SignatoryJobTitleSummary, SignatoryNameSummary}
+import viewmodels.govuk.summarylist.*
+import views.html.signatories.SignatoryCheckYourAnswersView
+
+import javax.inject.Inject
+
+class SignatoryCheckYourAnswersController @Inject()(
+  override val messagesApi: MessagesApi,
+  identify: IdentifierAction,
+  getData: DataRetrievalAction,
+  requireData: DataRequiredAction,
+  val controllerComponents: MessagesControllerComponents,
+  view: SignatoryCheckYourAnswersView
+) extends FrontendBaseController
+    with I18nSupport
+    with Logging {
+
+  // TODO: Create ticket to ensure CYA validates required journeyData before loading
+  def onPageLoad(id: String): Action[AnyContent] =
+    (identify andThen getData andThen requireData) { implicit request =>
+      Ok(view(SummaryListViewModel(buildSummaryRows(id))))
+    }
+
+  private def buildSummaryRows(id: String)(implicit request: DataRequest[_], messages: Messages) =
+    findSignatory(id).toSeq.flatMap { lo =>
+      Seq(
+        SignatoryNameSummary.row(lo),
+        SignatoryJobTitleSummary.row(lo)
+      ).flatten
+    }
+
+  private def findSignatory(id: String)(implicit request: DataRequest[_]): Option[Signatory] =
+    request.journeyData.signatories.flatMap(_.signatories.find(_.id == id))
+}
---
+++ test/controllers/signatories/SignatoryCheckYourAnswersControllerSpec.scala
@@ -0,0 +1,87 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.signatories
+
+import base.SpecBase
+import controllers.signatories.routes.SignatoryCheckYourAnswersController
+import models.journeydata.JourneyData
+import models.journeydata.signatories.{Signatories, Signatory}
+import play.api.test.FakeRequest
+import play.api.test.Helpers.*
+import viewmodels.checkAnswers.signatories.{SignatoryJobTitleSummary, SignatoryNameSummary}
+import viewmodels.govuk.summarylist.*
+import views.html.signatories.SignatoryCheckYourAnswersView
+
+class SignatoryCheckYourAnswersControllerSpec extends SpecBase {
+
+  private val existingId = "existing-id-123"
+  private val otherId    = "other-id-123"
+
+  lazy val routeUrl: String = SignatoryCheckYourAnswersController.onPageLoad(existingId).url
+
+  "SignatoryCheckYourAnswersController" - {
+
+    "must return OK and the correct view for a GET when the signatories exists" in {
+
+      val signatory =
+        Signatory(
+          id = existingId,
+          fullName = Some("Jane Smith"),
+          jobTitle = Some("Job Title")
+        )
+
+      val journeyData =
+        JourneyData(
+          groupId = testGroupId,
+          enrolmentId = testString,
+          signatories = Some(
+            Signatories(
+              Seq(
+                Signatory(
+                  id = otherId,
+                  fullName = Some("Other Person"),
+                  jobTitle = Some("Other Job Title")
+                ),
+                signatory
+              )
+            )
+          )
+        )
+
+      val application = applicationBuilder(journeyData = Some(journeyData)).build()
+
+      running(application) {
+        val request = FakeRequest(GET, routeUrl)
+        val result  = route(application, request).value
+
+        val view = application.injector.instanceOf[SignatoryCheckYourAnswersView]
+
+        val expectedRows =
+          Seq(
+            SignatoryNameSummary.row(signatory),
+            SignatoryJobTitleSummary.row(signatory)
+          ).flatten
+
+        status(result) mustEqual OK
+        contentAsString(result) mustEqual view(SummaryListViewModel(expectedRows))(
+          request,
+          messages(application)
+        ).toString
+      }
+    }
+  }
+}
