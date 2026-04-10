19b48df0528f0aead408d59c164fc273b58d030c
zachhall1234 <29452017+zachhall1234@users.noreply.github.com>
2026-04-10 16:19:26 +0100

    DFI-1110 added signatory full name page (#54)
    * DFI-1742 Add remove a signatory page
    
    * DFI-1742 increase test coverage
    
    * DFI-1742 scalafmt
    
    * DFI-1110 added signatory full name page
    
    * DFI-1110 create 'signatory name' page
    
    * DFI-1110 tidy up

---
+++ app/controllers/signatories/SignatoryNameController.scala
@@ -0,0 +1,109 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.signatories
+
+import controllers.actions.*
+import forms.SignatoryNameFormProvider
+import handlers.ErrorHandler
+import models.Mode
+import models.journeydata.signatories.{Signatories, Signatory}
+import navigation.Navigator
+import pages.signatories.SignatoryNamePage
+import play.api.Logging
+import play.api.data.Form
+import play.api.i18n.{I18nSupport, MessagesApi}
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import services.JourneyAnswersService
+import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
+import utils.UuidGenerator
+import views.html.signatories.SignatoryNameView
+import javax.inject.Inject
+import scala.concurrent.{ExecutionContext, Future}
+import scala.util.control.NonFatal
+
+class SignatoryNameController @Inject() (
+  override val messagesApi: MessagesApi,
+  identify: IdentifierAction,
+  getData: DataRetrievalAction,
+  requireData: DataRequiredAction,
+  auditContinuation: AuditContinuationAction,
+  journeyAnswersService: JourneyAnswersService,
+  errorHandler: ErrorHandler,
+  formProvider: SignatoryNameFormProvider,
+  navigator: Navigator,
+  uuidGenerator: UuidGenerator,
+  val controllerComponents: MessagesControllerComponents,
+  view: SignatoryNameView
+)(implicit ec: ExecutionContext)
+    extends FrontendBaseController
+    with I18nSupport
+    with Logging {
+
+  val form: Form[String] = formProvider()
+
+  def onPageLoad(id: Option[String], mode: Mode): Action[AnyContent] =
+    (identify andThen getData andThen requireData andThen auditContinuation(Signatories.sectionName)) {
+      implicit request =>
+
+        val preparedFormAndId = (for {
+          id          <- id
+          signatories <- request.journeyData.signatories.map(_.signatories)
+          signatory   <- signatories.find(_.id == id)
+          name        <- signatory.fullName
+        } yield (form.fill(name), id))
+          .getOrElse((form, uuidGenerator.generate()))
+
+        Ok(view(preparedFormAndId._2, preparedFormAndId._1, mode))
+    }
+
+  def onSubmit(id: String, mode: Mode): Action[AnyContent] = (identify andThen getData andThen requireData).async {
+    implicit request =>
+      form
+        .bindFromRequest()
+        .fold(
+          formWithErrors => Future.successful(BadRequest(view(id, formWithErrors, mode))),
+          answer => {
+            val existingSection   = request.journeyData.signatories
+            val upsertedSignatory = Signatory(id, Some(answer))
+            val updatedSection    = existingSection match {
+              case Some(existing) =>
+                val existingSignatory = existing.signatories.filter(_.id != id)
+                existing.copy(signatories = existingSignatory :+ upsertedSignatory)
+              case None           => Signatories(Seq(upsertedSignatory))
+            }
+
+            journeyAnswersService
+              .update(updatedSection, request.groupId, request.credentials.providerId)
+              .map { updatedSection =>
+                Redirect(
+                  navigator.nextPage(
+                    SignatoryNamePage,
+                    updatedSection,
+                    mode
+                  )
+                )
+              }
+              .recoverWith { case NonFatal(e) =>
+                logger.warn(
+                  s"Failed updating answers for section [${Signatories.sectionName}] for groupId [${request.groupId}] with error: [$e]"
+                )
+                errorHandler.internalServerError
+              }
+          }
+        )
+  }
+}
