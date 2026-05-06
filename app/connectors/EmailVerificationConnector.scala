836ca2235b9b439dff804f9b66859d29f1dff8ad
Tom Robinson <25112739+magicaljellybeans@users.noreply.github.com>
2026-05-06 11:20:19 +0100

    DFI-1994 Implementation of organisation email address page and email verification code request

---
+++ app/connectors/EmailVerificationConnector.scala
@@ -0,0 +1,86 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package connectors
+
+import config.FrontendAppConfig
+import models.emailverification.{SendEmailVerificationCodeRequest, VerifyEmailCodeRequest}
+import play.api.http.Status.{INTERNAL_SERVER_ERROR, OK}
+import play.api.libs.json.Json
+import play.api.libs.ws.JsonBodyWritables.writeableOf_JsValue
+import uk.gov.hmrc.http.client.HttpClientV2
+import uk.gov.hmrc.http.{HeaderCarrier, HttpResponse, StringContextOps, UpstreamErrorResponse}
+import uk.gov.hmrc.http.HttpReads.Implicits._
+
+import javax.inject.Inject
+import scala.concurrent.{ExecutionContext, Future}
+
+class EmailVerificationConnector @Inject() (
+  httpClient: HttpClientV2,
+  appConfig: FrontendAppConfig
+)(implicit ec: ExecutionContext) {
+
+  def sendCode(email: String)(implicit hc: HeaderCarrier): Future[Unit] = {
+    val url = s"${appConfig.emailVerificationBaseUrl}/v2/send-code"
+
+    httpClient
+      .post(url"$url")
+      .withBody(Json.toJson(SendEmailVerificationCodeRequest(email)))
+      .execute[HttpResponse]
+      .map { response =>
+        response.status match {
+          case OK =>
+            ()
+
+          case status =>
+            throw UpstreamErrorResponse(
+              message = upstreamErrorMessage(
+                endpoint = "POST /v2/send-code",
+                status = response.status,
+                body = response.body
+              ),
+              statusCode = response.status,
+              reportAs = INTERNAL_SERVER_ERROR
+            )
+        }
+      }
+  }
+
+  def verifyCode(email: String, code: String)(implicit hc: HeaderCarrier): Future[Unit] = {
+    val url = s"${appConfig.emailVerificationBaseUrl}/v2/verify-code"
+
+    httpClient
+      .post(url"$url")
+      .withBody(Json.toJson(VerifyEmailCodeRequest(email, code)))
+      .execute[HttpResponse]
+      .map { response =>
+        response.status match {
+          case OK =>
+            ()
+
+          case status =>
+            throw UpstreamErrorResponse(
+              message = s"Email verification verify-code failed with status [$status]",
+              statusCode = status,
+              reportAs = INTERNAL_SERVER_ERROR
+            )
+        }
+      }
+  }
+
+  private def upstreamErrorMessage(endpoint: String, status: Int, body: String): String =
+    s"Email verification call [$endpoint] failed with upstream status [$status] and body [$body]"
+}
